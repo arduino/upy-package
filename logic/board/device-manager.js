b7948c7aaa38cbfa02c79ecb709c21aa9d669138
Sebastian Romero <s.romero@arduino.cc>
2024-11-15 10:20:33 +0100

    Rename board manager

--- logic/board/device-manager.js
+++ logic/board/device-manager.js
@@ -1,12 +1,12 @@
-import { execSync } from 'child_process';
 import inquirer from 'inquirer';
-import { SerialDevice, SerialDeviceFinder } from './serialDeviceFinder.js';
+import { SerialDeviceFinder } from './serialDeviceFinder.js';
 import * as descriptors from './descriptors.js';
+import { getMicroPythonVersionFromPort } from 'upy-packager';
 
 /**
  * Class to manage connected boards
  */
-export class BoardManager {
+export class DeviceManager {
 
     constructor() {
         this.serialDeviceFinder = new SerialDeviceFinder();
@@ -14,10 +14,10 @@ export class BoardManager {
     }
 
     /**
-     * Get a list of connected boards. It uses mpremote to get the list.
-     * @returns {Array} List of connected boards in the format {port, ID, vendorID, productID, name}
+     * Get a list of connected boards. Optionally filter by vendorID and productID.
+     * @returns {Promise<Array>} List of connected boards in the format {port, ID, vendorID, productID, name}
      */
-    async getConnectedBoards(vendorIDFilter = null, productIDFilter = null) {
+    async getConnectedDevices(vendorIDFilter = null, productIDFilter = null) {
         let devices = await this.serialDeviceFinder.getDeviceList(vendorIDFilter, productIDFilter);
         devices = devices.map((device) => {
             const matchingDescriptors = this.deviceDescriptors.filter((descriptor) => {
@@ -42,8 +42,8 @@ export class BoardManager {
      * Gets the board based on the vendorID. If no vendorID is provided, it will return the first board found.
      * If more than one board is found, it will prompt the user to select a board.
      */
-    async getBoard(vendorIDFilter = null, productIDFilter = null) {
-        let boards = await this.getConnectedBoards(vendorIDFilter, productIDFilter);
+    async getDevice(vendorIDFilter = null, productIDFilter = null) {
+        let boards = await this.getConnectedDevices(vendorIDFilter, productIDFilter);
         
         if (boards.length === 0) {
             return null;
@@ -68,10 +68,7 @@ export class BoardManager {
         return selection.selectedPort;
     }
 
-    getMicroPythonVersion(board) {
-        // TODO: Implement this
-        // const command = `mpremote connect id:${board.ID} exec "import os; print(os.uname().release)"`;
-        // return execSync(command, { encoding: 'utf-8' }).trim();
-        return null;
+    async getMicroPythonVersion(serialDevice) {
+        return getMicroPythonVersionFromPort(serialDevice.serialPort);
     }
 }
